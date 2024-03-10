1e94a0a8a5e93e5e64d2205840ae6343790956af
dbbz <coder@dbenbouzid.com>
2024-03-10 22:25:17 +0100

    Add password protections. Comment some sections. Add upload to GSheet.

--- ai_risk_taxonomy/app.py
+++ ai_risk_taxonomy/app.py
@@ -3,9 +3,12 @@ import pandas as pd
 import shelve
 from streamlit_gsheets import GSheetsConnection
 import datetime
+import hmac
 
+# The list of annotators (or the initials thereof)
 annotators = ["CP", "DB", "EP", "JH", "GA", "HP", "LW", "MS", "PN", "TC", "TD", "US"]
 
+# The list of harm categories and sub-categories
 harm_categories = {
     "Autonomy": [
         "Autonomy/agency loss",
@@ -86,6 +89,7 @@ harm_categories = {
     "Other": ["Cheating/plagiarism"],
 }
 
+# The list of impacted stakeholders
 stakeholders = {
     "Users": "Individuals or entities directly interacting with a system, or those being directly targeted by it, such as citizens, consumers, patients, students, employees, delivery drivers, job applicants, travellers, immigrants",
     "General public": "Individuals indirectly impacted by a system, such as passers-by, local communities and other members of the general public",
@@ -97,25 +101,28 @@ stakeholders = {
     "Investors": "Shareholders/investors in the developer and/or deployer",
 }
 
+# Make a string containing the stakeholders descriptions
 stakeholders_help_text = ""
 for k, v in stakeholders.items():
     stakeholders_help_text += f"- **{k}**: {v}\n"
 
 
+# Load the incidents descriptions and related links
+# scrapped from the AIAAIC website as they are not in the sheet
 @st.cache_data
 def load_extra_data():
     with shelve.open("description", "r") as db:
-        # df_descriptions = pd.DataFrame.from_dict(
-        #     dict(db), columns=["description"], orient="index"
-        # )
         descriptions = dict(db)
     with shelve.open("links", "r") as db:
-        # df_links = pd.DataFrame.from_dict(dict(db), columns=["links"], orient="index")
         links = dict(db)
 
     return descriptions, links
 
 
+# Load the actual AIAAIC repository (list of incidents)
+# It used to be downloaded from the online repo
+# but due to frequent changes in the sheet format
+# I ended up using an offline (potentially not up to date) version
 @st.cache_data
 def load_incidents_repository():
     df = pd.read_csv("repository.csv").dropna(how="all")
@@ -124,23 +131,51 @@ def load_incidents_repository():
     return df
 
 
-with st.empty():
-    st.title("AI Risk Taxonomy")
+# Password protect to avoid annotation vandalism
+def check_password():
+    """Returns `True` if the user had the correct password."""
 
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
+st.title("AI Risk Taxonomy")
+with st.expander("📖 Instructions"):
+    st.markdown("""
+    👋🏼  Thank you for taking part in the AI Risk Taxonomy project.
+
+    - Instruction 1
+    - Instruction 2
+    - ...
+    """)
+
+# Connect to the Google Sheet where to store the answers
 try:
     conn = st.connection("gsheets", type=GSheetsConnection)
 except Exception as e:
     st.error("Cannot connect to Google Sheet. Error: " + str(e))
 
-data = conn.read(
-    worksheet="Annotations",
-    # usecols=[0, 1],
-    ttl="5m",
-)
-st.dataframe(data)
-
-
-results = []
 
 with st.container(border=True):
     st.markdown("Your name initials")
@@ -150,6 +185,7 @@ with st.container(border=True):
 
     if not user:
         st.stop()
+
     repository = load_incidents_repository()
     descriptions, links = load_extra_data()
 
@@ -204,14 +240,17 @@ with st.container(border=True):
         stakeholders.keys(),
         default=None,
         label_visibility="collapsed",
+        key=incident + "__impacted",
     )
     if not impacted_stakeholder:
         st.stop()
 
+results = {}
 for stakeholder in impacted_stakeholder:
+    results[stakeholder] = {}
     with st.container(border=True):
         st.markdown(
-            f"What category of harm impacts `{stakeholder}`? *(multiple options are possible)*",
+            f"What :violet[category] of harm impacts `{stakeholder}`? *(multiple options are possible)*",
             help="Stated actual negative impact(s) of incident/issue",
         )
         harm_category = st.multiselect(
@@ -219,14 +258,14 @@ for stakeholder in impacted_stakeholder:
             harm_categories.keys(),
             default=None,
             label_visibility="collapsed",
-            key="harm_category_" + stakeholder,
+            key=f"{incident}__{stakeholder}__harm_category",
         )
         if not harm_category:
             st.stop()
 
         for harm_cat in harm_category:
             st.markdown(
-                f"What specific `{harm_cat}` harm impacts `{stakeholder}`? *(multiple options are possible)*",
+                f"What :orange[specific] `{harm_cat}` harm impacts `{stakeholder}`? *(multiple options are possible)*",
                 help="Stated specific negative impact(s) of incident/issue",
             )
             harm_subcategory = st.multiselect(
@@ -234,13 +273,51 @@ for stakeholder in impacted_stakeholder:
                 harm_categories[harm_cat],
                 default=None,
                 label_visibility="collapsed",
-                key="harm_subcategory_" + stakeholder + harm_cat,
+                key=f"{incident}__{stakeholder}__{harm_cat}__harm_subcategory",
             )
             if not harm_subcategory:
                 st.stop()
+            results[stakeholder][harm_cat] = harm_subcategory
 
 submitted = st.button("Submit your answers", type="primary", use_container_width=True)
 if submitted:
-    current_datetime = datetime.datetime.now()
-    st.write(current_datetime)
+    # current_datetime = datetime.datetime.now()
+    current_datetime = datetime.date.today()
+    timestamp = int(datetime.datetime.now().timestamp())
+    tabular_results = []
+    for stakeholder, harm in results.items():
+        for harm_cat, harm_subcat_list in harm.items():
+            for harm_subcat in harm_subcat_list:
+                tabular_results.append(
+                    [
+                        current_datetime,
+                        user,
+                        incident,
+                        stakeholder,
+                        harm_cat,
+                        harm_subcat,
+                        timestamp,
+                    ]
+                )
+
+    columns = [
+        "datatime",
+        "annotator",
+        "incident_ID",
+        "stakeholders",
+        "harm_category",
+        "harm_subcategory",
+        "timestamp",
+    ]
+    df_update = pd.DataFrame(data=tabular_results, columns=columns)
+
+    with st.spinner("Writing to Google Sheet..."):
+        df = conn.read(worksheet="Annotations", ttl=0, usecols=columns).dropna()
+        df = pd.concat([df, df_update], ignore_index=True)
+        conn.update(worksheet="Annotations", data=df)
+
     st.balloons()
+    st.toast("Your answers were submitted.")
+    st.toast("You can select another incident to annotate.")
+
+    # Clear cache?
