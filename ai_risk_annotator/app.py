bcb250a0f39759e98cab8b7fadc13021cf6c2eb9
dbbz <coder@dbenbouzid.com>
2024-03-12 21:37:52 +0100

    Limit the incidents to the ones on a text file. Scrap the description and links from the web. Show the general taxonomy on a mindmap. Improve the UI by displaying a disabled button in case some questions are unanswered.

--- ai_risk_annotator/app.py
+++ ai_risk_annotator/app.py
@@ -1,10 +1,16 @@
-import streamlit as st
-import pandas as pd
-import shelve
-import pickle
-from streamlit_gsheets import GSheetsConnection
 import datetime
 import hmac
+import pickle
+import re
+import shelve
+
+import pandas as pd
+import requests
+import streamlit as st
+from bs4 import BeautifulSoup
+from streamlit_gsheets import GSheetsConnection
+from streamlit_markmap import markmap
+from markdownify import markdownify
 
 st.set_page_config(page_title="AI Harm Annotator", layout="wide")
 # The list of annotators (or the initials thereof)
@@ -134,11 +140,78 @@ def load_extra_data():
 # but due to frequent changes in the sheet format
 # I ended up using an offline (potentially not up to date) version
 @st.cache_data
-def load_incidents_repository():
-    df = pd.read_csv("repository.csv").dropna(how="all")
-    df = df.set_index(df.columns[0])[["Headline/title", "Description/links"]]
+def read_incidents_repository_from_file():
+    df = (
+        pd.read_csv("repository.csv", skip_blank_lines=True, skiprows=[0, 2])
+        .dropna(how="all")
+        .dropna(axis=1, how="all")
+    )
+
+    df = df.set_index(df.columns[0]).rename(columns=lambda x: x.strip())[
+        ["Headline/title", "Description/links"]
+    ]
+
     df.columns = ["title", "links"]
     return df
+
+
+@st.cache_data
+def download_incidents_repository():
+    AIAAIC_SHEET_ID = "1Bn55B4xz21-_Rgdr8BBb2lt0n_4rzLGxFADMlVW0PYI"
+    AIAAIC_SHEET_NAME = "Repository"
+
+    url = f"https://docs.google.com/spreadsheets/d/{AIAAIC_SHEET_ID}/gviz/tq?tqx=out:csv&sheet={AIAAIC_SHEET_NAME}"
+    df = (
+        pd.read_csv(url, skip_blank_lines=True)
+        .dropna(how="all")
+        .dropna(axis=1, how="all")
+    )
+    df
+    df = df.set_index(df.columns[0]).rename(columns=lambda x: x.strip())[
+        ["Headline/title", "Description/links"]
+    ]
+
+    df.columns = ["title", "links"]
+    return df
+
+
+@st.cache_data(show_spinner="Fetching more information about the incident...")
+def scrap_incident_description(link):
+    soup = BeautifulSoup(requests.get(link).text, "html.parser")
+
+    # This is dangeriously hard-coded.
+    description = soup.find_all(
+        class_="hJDwNd-AhqUyc-uQSCkd Ft7HRd-AhqUyc-uQSCkd jXK9ad D2fZ2 zu5uec OjCsFc dmUFtb wHaque g5GTcb"
+    )
+    description = description[1].get_text()
+    return description
+
+
+def get_deepest_text(tag):
+    if tag.string:
+        return tag.string.strip()  # Return text if it's a text node
+
+    # Recursive search for text within children
+    for child in tag.children:
+        text = get_deepest_text(child)
+        if text:
+            return text
+
+    return None
+
+
+@st.cache_data(show_spinner="Fetching the list of links on the incident...")
+def get_list_of_media_links(page_url):
+    soup = BeautifulSoup(requests.get(page_url).text, "html.parser")
+    section = soup.find(string=re.compile(", commentar"))
+    if not section:
+        section = soup.find(string=re.compile("act check 🚩"))
+    if section:
+        li_list = section.find_next("ul").find_all("li")
+        # links = [get_deepest_text(li) for li in li_list]
+        return markdownify("\n".join(str(i) for i in li_list))
+    else:
+        return ""
 
 
 # Password protect to avoid annotation vandalism
@@ -167,7 +240,7 @@ def check_password():
 
 
 if not check_password():
-    st.stop()  # Do not continue if check_password is not True.
+    st.stop()
 
 
 st.title("✍🏻 AI Harm Annotator")
@@ -178,12 +251,12 @@ try:
 except Exception as e:
     st.error("Cannot connect to Google Sheet. Error: " + str(e))
 
-df_taxonomy = (
-    conn.read(worksheet="Taxonomy", ttl=10).dropna(how="all").dropna(how="all", axis=1)
-)
+# df_taxonomy = (
+#     conn.read(worksheet="Taxonomy", ttl=10).dropna(how="all").dropna(how="all", axis=1)
+# )
 
 
-with st.expander("📖 Instructions"):
+with st.expander("📖 Instructions", expanded=False):
     st.markdown("""
     👋🏼  Thank you for taking part in the AI Risk Taxonomy project.
 
@@ -191,7 +264,22 @@ with st.expander("📖 Instructions"):
     - Instruction 2
     - ...
     """)
-    st.dataframe(df_taxonomy, use_container_width=True, hide_index=True)
+
+    taxonomy_mindmap = """
+---
+markmap:
+  colorFreezeLevel: 2
+---
+# AI Harm Taxonomy
+"""
+    for k, v in harm_categories.items():
+        taxonomy_mindmap += f"## {k}\n"
+        for i in v:
+            taxonomy_mindmap += f"### {i}\n"
+
+    st.markdown("#### Overview of the AI Harm Taxonomy")
+    markmap(taxonomy_mindmap)
+    # st.dataframe(df_taxonomy, use_container_width=True, hide_index=True)
 
 with st.container(border=True):
     st.markdown("Your name initials")
@@ -202,19 +290,26 @@ with st.container(border=True):
     if not user:
         st.stop()
 
-    repository = load_incidents_repository()
+    repository = read_incidents_repository_from_file()
     descriptions, links = load_extra_data()
 
-    # User the URL parameters to filter the incidents
-    # to be annotated
-    # eg. for <app_url>/?id=AIAAIC1366&id=AIAAIC1365
-    # only the incidents with the ids AIAAIC1366 and AIAAIC1365
-    # will be shown.
-    filtered_incidents = st.query_params.get_all("id")
-    if filtered_incidents and set(repository.index) & set(filtered_incidents):
-        incidents_list = filtered_incidents
-    else:
-        incidents_list = list(repository.index)
+    with open("shortlist.txt", "r") as f:
+        incidents_list = [line.strip() for line in f.readlines()]
+        incidents_list = set(incidents_list) & set(repository.index)
+        incidents_list = list(incidents_list)
+
+    if not incidents_list:
+        # User the URL parameters to filter the incidents
+        # to be annotated
+        # eg. for <app_url>/?id=AIAAIC1366&id=AIAAIC1365
+        # only the incidents with the ids AIAAIC1366 and AIAAIC1365
+        # will be shown.
+        filtered_incidents = st.query_params.get_all("id")
+        if filtered_incidents and set(repository.index) & set(filtered_incidents):
+            incidents_list = set(repository.index) & set(filtered_incidents)
+            incidents_list = list(filtered_incidents)
+        else:
+            incidents_list = list(repository.index)
 
     st.markdown("Select an incident")
     incident = st.selectbox(
@@ -229,18 +324,23 @@ with st.container(border=True):
 st.markdown("#### Incident description")
 
 with st.container(border=True):
+    incident_page = repository.loc[incident, "links"]
     st.markdown("##### " + repository.loc[incident, "title"])
     if incident in descriptions:
         st.info(descriptions[incident])
     else:
-        st.warning("Description missing.")
+        # st.warning("Description missing.")
+        st.info(scrap_incident_description(incident_page))
     st.markdown("##### Links")
-    if incident in links:
-        st.write("\n".join(map(lambda x: "- " + x, links[incident])))
+    # if incident in links:
+    #     st.write("\n".join(map(lambda x: "- " + x, links[incident])))
+    # else:
+
+    st.write(get_list_of_media_links(incident_page))
 
     st.link_button(
         "Go to the incident page",
-        repository.loc[incident, "links"],
+        incident_page,
         use_container_width=True,
     )
 
@@ -258,13 +358,21 @@ with st.container(border=True):
         label_visibility="collapsed",
         key=incident + "__impacted",
     )
-    if not impacted_stakeholder:
-        st.stop()
+
+if not impacted_stakeholder:
+    submitted = st.button(
+        "Submit your answers",
+        type="primary",
+        use_container_width=True,
+        disabled=True,
+    )
+    st.stop()
 
 results = {}
 for stakeholder in impacted_stakeholder:
     results[stakeholder] = {}
-    with st.container(border=True):
+    harm_category_section = st.container(border=True)
+    with harm_category_section:
         st.markdown(
             f"What :violet[category] of harm impacts `{stakeholder}`? *(multiple options are possible)*",
             help="Stated actual negative impact(s) of incident/issue",
@@ -276,10 +384,17 @@ for stakeholder in impacted_stakeholder:
             label_visibility="collapsed",
             key=f"{incident}__{stakeholder}__harm_category",
         )
-        if not harm_category:
-            st.stop()
+    if not harm_category:
+        submitted = st.button(
+            "Submit your answers",
+            type="primary",
+            use_container_width=True,
+            disabled=True,
+        )
+        st.stop()
 
-        for harm_cat in harm_category:
+    for harm_cat in harm_category:
+        with harm_category_section:
             st.markdown(
                 f"What :orange[specific] `{harm_cat}` harm impacts `{stakeholder}`? *(multiple options are possible)*",
                 help="Stated specific negative impact(s) of incident/issue",
@@ -299,9 +414,15 @@ for stakeholder in impacted_stakeholder:
                     key=f"{incident}__{stakeholder}__{harm_cat}__notes",
                 )
 
-            if not harm_subcategory:
-                st.stop()
-            results[stakeholder][harm_cat] = (harm_subcategory, notes)
+        if not harm_subcategory:
+            submitted = st.button(
+                "Submit your answers",
+                type="primary",
+                use_container_width=True,
+                disabled=True,
+            )
+            st.stop()
+        results[stakeholder][harm_cat] = (harm_subcategory, notes)
 
 
 submitted = st.button("Submit your answers", type="primary", use_container_width=True)
--- ai_risk_annotator/pages/analysis.py
+++ ai_risk_annotator/pages/analysis.py
@@ -1,3 +1,38 @@
 import streamlit as st
+import pandas as pd
+import shelve
+import pickle
+from streamlit_gsheets import GSheetsConnection
+import datetime
+import hmac
 
-st.info("WIP")
+
+st.title("Agreement calculation")
+
+# Connect to the Google Sheet where to store the answers
+try:
+    conn = st.connection("gsheets", type=GSheetsConnection)
+except Exception as e:
+    st.error("Cannot connect to Google Sheet. Error: " + str(e))
+
+columns = [
+    "datatime",
+    "annotator",
+    "incident_ID",
+    "stakeholders",
+    "harm_category",
+    "harm_subcategory",
+    "notes",
+    "timestamp",
+]
+
+# df = conn.read(worksheet="Annotations", ttl=0, usecols=columns).dropna()
+# st.dataframe(df, use_container_width=True, hide_index=True)
+
+AIAAIC_SHEET_ID = "1Bn55B4xz21-_Rgdr8BBb2lt0n_4rzLGxFADMlVW0PYI"
+AIAAIC_SHEET_NAME = "Repository"
+
+url = f"https://docs.google.com/spreadsheets/d/{AIAAIC_SHEET_ID}/gviz/tq?tqx=out:csv&sheet={AIAAIC_SHEET_NAME}"
+df_ = pd.read_csv(url, skip_blank_lines=True).dropna(how="all")
+
+st.write(df_)
